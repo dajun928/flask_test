9f6fdf8320cd63ebc84c8655142f147d0f2490ec
dajun928 <1663177102@qq.com>
2019-07-03 22:50:41 +0800

    test

---
+++ my_flask.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-  
+"""
+@func : 阿里云部署Flask项目
+@file : my_flask.py
+@time : 2019/07/02 00:42:13
+@url : https://www.jianshu.com/p/9293cc21a571?utm_campaign=maleskine&utm_content=note&utm_medium=seo_notes&utm_source=recommendation
+"""
+import platform
+
+print(platform.python_version())
