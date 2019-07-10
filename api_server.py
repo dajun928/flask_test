b81470970080bc8cd04f709620930aca4faf3cba
dajun928 <1663177102@qq.com>
2019-07-10 23:16:12 +0800

    add

---
+++ api_server.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-  
+"""
+@func : 阿里云部署Flask项目
+@file : my_flask.py
+@time : 2019/07/02 00:42:13
+@url : https://www.jianshu.com/p/9293cc21a571?utm_campaign=maleskine&utm_content=note&utm_medium=seo_notes&utm_source=recommendation
+"""
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+@version : 
+@file : test.py
+@time : 2019/06/19 23:35:08
+@func : 
+"""
+#导入Flask扩展
+from flask import Flask,render_template
+
+# 创建Flask应用实例
+app = Flask(__name__)
+
+# 定义路由及视图函数
+@app.route('/')
+def hello_world():
+    return 'Hello world!'
+
+
+# 启动程序
+if __name__ == '__main__':
+    app.run(debug=True)
